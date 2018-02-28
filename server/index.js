7d99659f7dd3c0d1a2db3c4fe6f72f0098710731
Nick Leung <nickleung+bitbucket@gmail.com>
2018-02-28 13:53:19 -0800

    Added server.

---
+++ server/index.js
@@ -0,0 +1,15 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var mongoose = require('mongoose');
+var availableDateRouter = require('./routers/availableDate.js');
+
+var app = express();
+mongoose.connect('mongodb://localhost/experiences');
+
+app.use(bodyParser.json());
+//app.use(express.static(__dirname + '/../react-client/dist'));
+app.use('/experience/availableDate', availableDateRouter);
+
+app.listen(8000, function() {
+  console.log('listening on port 8000');
+});
