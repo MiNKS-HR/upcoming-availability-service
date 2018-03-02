244c9be51434b77bfcb44d22325de9fc902b7593
Nick Leung <nickleung+bitbucket@gmail.com>
2018-03-01 18:26:24 -0800

    Modify front end to utilize axios and stylesheet

--- client/src/components/AvailableDate.jsx
+++ client/src/components/AvailableDate.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
-
-var entryStyle = {
-  button: {
-    float: 'right'
-  },
-  text: {
-    float: 'left',
-    lineHeight: '30px'
-  },
-  outer: {
-    borderBottomWidth: '1px', 
-    borderBottomStyle: 'solid', 
-    borderBottomColor: 'gray',
-    height: '30px'
-  }
-};
+import AvailableDateStyles from './AvailableDate.css';
 
 class AvailableDate extends React.Component {
   constructor(props) {
@@ -26,15 +11,15 @@ class AvailableDate extends React.Component {
 
     return (
       <div>
-        <div style={entryStyle.outer}>
-          <div style={entryStyle.text}>{availableDate.id}</div>
-          <div style={entryStyle.text}>{availableDate.starts_at}</div>
-          <div style={entryStyle.text}>{availableDate.ends_at}</div>
-          <div style={entryStyle.text}>{availableDate.price_per_guest}</div>
-          <div style={entryStyle.text}>{availableDate.max_guests}</div>
-          <div style={entryStyle.text}>{availableDate.remaining_capacity}</div>
-
-          <button type="button" style={entryStyle.button}>Choose</button>
+        <div className="outer">
+          <div className="text">
+          {availableDate.id}
+          {availableDate.starts_at}
+          {availableDate.ends_at}
+          {availableDate.price_per_guest}
+          {availableDate.max_guests}
+          {availableDate.remaining_capacity}</div>
+          <button type="button" className="button">Choose</button>
         </div>
       </div>
     )
