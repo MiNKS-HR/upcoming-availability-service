80765b42a46111f42457a5fbf3c823f1183c680e
Nick Leung <nickleung+bitbucket@gmail.com>
2018-03-09 14:54:24 -0800

    Tweaks to docker set up and index html.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM node:8.9.4
 RUN mkdir -p /user/src/available
 WORKDIR /user/src/available
-RUN ls
 COPY . /user/src/available
-RUN ls
 RUN npm install
 EXPOSE 3002
 CMD npm run docker
--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,7 @@ var data = require('./seed_data.js');
 var mongoose = require('mongoose');
 var AvailableDates = require('./models/availableDate.js');
 
-mongoose.connect('mongodb://database/experiences');
+mongoose.connect('mongodb://localhost/experiences');
 
 var seedDb = function (data) {
   AvailableDates.insertOne(data, function (err, response) {
@@ -11,14 +11,6 @@ var seedDb = function (data) {
     } else {
       console.log('Saved successfully to the database: ' + response);
     }
-    // mongoose.connection.close(function () {
-    //   if (err) {
-    //     console.log('Error inserting into database: ' + err);
-    //   } else {
-    //     console.log('Saved successfully to the database: ' + response);
-    //   }
-    //   console.log("Connection closed.");
-    // });
   });
 };
 
